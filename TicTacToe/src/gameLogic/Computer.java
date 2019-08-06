d93ada69608c38544f536c8d7d748de23523aec9
wei.fu <wei.fu@B106-19.soit.local>
2019-08-07 09:40:39 +1200

    Initial Commit with Skeleton Code

---
+++ TicTacToe/src/gameLogic/Computer.java
@@ -0,0 +1,7 @@
+package gameLogic;
+
+public class Computer {
+	
+	//computerMove(2d array)
+
+}
---
+++ TicTacToe/src/gameLogic/GameLogic.java
@@ -0,0 +1,17 @@
+package gameLogic;
+
+public class GameLogic {
+	//2d array for the board
+	//computer object
+    
+	public static void main(String[] args) {
+		
+
+		
+
+	}
+	//executeMove(2d array)
+	//checkBoardCondition(2d array)
+	//returnBoardState  /return boardUpdate   communicate back with servlet
+
+}
