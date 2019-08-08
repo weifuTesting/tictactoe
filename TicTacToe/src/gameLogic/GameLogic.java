8cdf29c5406a4167d667bf8fb34a55796db852cf
wei.fu <wei.fu@B204-12.soit.local>
2019-08-08 14:58:44 +1200

    Got Horizontal and Vertical checking done need to do diagonal checking now

--- TicTacToe/src/gameLogic/GameLogic.java
+++ TicTacToe/src/gameLogic/GameLogic.java
@@ -1,16 +1,56 @@
 package gameLogic;
 
-public class GameLogic {
-	//2d array for the board
+public class GameLogic {           //2d array for the board
+	static int board[][] = new int[][] {{0,0,0},
+					 					{0,0,0},  //board[x axis][y axis] 
+					 					{0,0,0}}; //0 for empty, 1 for player,2 for opponent
+	static int rows = 3;
+	static int collumns = 3;
+	
 	//computer object
     
 	public static void main(String[] args) {
+		//System.out.println(board[0][1]+" ");
+		System.out.println(CheckBoardCondition());
 		
 
 		
 
 	}
 	//executeMove(2d array)
+	
+	public static int CheckBoardCondition(){
+		boolean playerWin = false;
+		boolean computerWin = false;
+		
+		
+		//horizontal
+		for (int i = 0; i < collumns; i++) {
+			if ((board[i][0] == 1) && (board[i][1] == 1) && (board[i][2] == 1)) 
+				playerWin = true;
+			
+			if ((board[i][0] == 2) && (board[i][1] == 2) && (board[i][2] == 2)) 
+				computerWin = true;								
+			//return true;		
+		}
+		//vertical
+		for (int i = 0; i < rows; i++) {
+			if ((board[0][i] == 1) && (board[1][i] == 1) && (board[2][i] == 1)) 
+				playerWin = true;
+			
+			if ((board[0][i] == 2) && (board[1][i] == 2) && (board[2][i] == 2)) 
+				computerWin = true;								
+		}
+		
+		if (playerWin == true)
+			return 1;
+		else if(computerWin == true)
+			return 2;
+		else 
+			return 0;
+		
+		
+	}
 	//checkBoardCondition(2d array)
 	//returnBoardState  /return boardUpdate   communicate back with servlet
 
