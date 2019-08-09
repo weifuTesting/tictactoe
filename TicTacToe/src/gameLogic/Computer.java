60a55d91bb58b224336d9eeed3f6815987763ae5
wei.fu <wei.fu@B108-02.soit.local>
2019-08-09 16:46:41 +1200

    cont...

--- TicTacToe/src/gameLogic/Computer.java
+++ TicTacToe/src/gameLogic/Computer.java
@@ -2,6 +2,12 @@ package gameLogic;
 
 public class Computer {
 	
+	//array 0 = empty
+	//      1 = player
+	//		2 = computer
+	
 	//computerMove(2d array)
+	//randomly picks an empty spot in the array (if it's not empty retry)
+	//inserts a 2 into the empty spot
 
 }
